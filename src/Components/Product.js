37c655684d7a4bb8616f4aeb95081cdecde76492
Klay Clarke <klayaclarke@gmail.com>
2022-02-25 00:15:38 -0500

    add number input field that allows user to specify desired qty of certain items before adding them to cart

--- src/Components/Product.js
+++ src/Components/Product.js
@@ -6,6 +6,7 @@ export default function Product(props) {
       <h4 className="product-name">{product.name}</h4>
       <div>${product.price}</div>
       <div>
+        <input type="number" min={"1"} id={product.id}></input>
         <button onClick={() => onAdd(product)}>Add To Cart</button>
       </div>
     </div>

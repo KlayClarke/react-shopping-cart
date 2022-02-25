6b4816d3baa05f517ee1c9a62182e9041e71c727
Klay Clarke <klayaclarke@gmail.com>
2022-02-24 21:31:00 -0500

    add product data to use in webpage

---
+++ src/data.js
@@ -0,0 +1,41 @@
+const data = {
+  products: [
+    {
+      id: 1,
+      name: "cotton breathable shirt women",
+      price: 12,
+      image:
+        "https://s7.landsend.com/is/image/LandsEnd/411450_SS19_FF_OZP?fmt=jpeg,rgb&qlt=60,1&op_sharpen=0&resMode=sharp2&op_usm=0.5,1,3,0&icc=sRGB%20IEC61966-2.1,relative&iccEmbed=1&hei=2061&wid=1374",
+    },
+    {
+      id: 2,
+      name: "cotton breathable pants women",
+      price: 26,
+      image:
+        "https://image.uniqlo.com/UQ/ST3/WesternCommon/imagesgoods/443401/item/goods_58_443401.jpg?width=734",
+    },
+    {
+      id: 3,
+      name: "bag pants women",
+      price: 26,
+      image:
+        "https://img.joomcdn.net/ce5999ab440cbcd13a6678eea3f3c160257b5d6e_original.jpeg",
+    },
+    {
+      id: 4,
+      name: "circle shades",
+      price: 43,
+      image:
+        "https://cdn.shopify.com/s/files/1/0193/6253/products/2706352897_1600x.jpg?v=1638487619",
+    },
+    {
+      id: 5,
+      name: "matric shades",
+      price: 30,
+      image:
+        "https://cdn11.bigcommerce.com/s-dopm3h2gdf/images/stencil/1280x1280/attribute_rule_images/397_source_1573912664.jpg",
+    },
+  ],
+};
+
+export default data;

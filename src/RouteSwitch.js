06573846a4d62f45cd4907b77d94fefdd4695b27
Klay Clarke <klayaclarke@gmail.com>
2022-02-24 22:31:43 -0500

    setup state instance for cartItems, pass cartItems to the cart route - create functions that handling the addition and removal of products to and from the cart - passing these as props

--- src/RouteSwitch.js
+++ src/RouteSwitch.js
@@ -1,4 +1,5 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { useState } from "react";
 import App from "./App";
 import Main from "./Components/Main";
 import Cart from "./Components/Cart";
@@ -6,14 +7,50 @@ import Header from "./Components/Header";
 import data from "./data";
 
 export default function RouteSwitch() {
+  const [cartItems, setCartItems] = useState([]);
+
   const { products } = data;
+
+  function onAdd(product) {
+    const exist = cartItems.find((x) => x.id === product.id);
+    if (exist) {
+      setCartItems(
+        cartItems.map((x) =>
+          x.id === product.id ? { ...exist, qty: exist.qty + 1 } : x
+        )
+      );
+    } else {
+      setCartItems([...cartItems, { ...product, qty: 1 }]);
+    }
+  }
+
+  function onRemove(product) {
+    const exist = cartItems.find((x) => x.id === product.id);
+    if (exist) {
+      setCartItems(
+        cartItems.map((x) =>
+          x.id === product.id && x.qty > 0
+            ? { ...exist, qty: exist.qty - 1 }
+            : x
+        )
+      );
+    }
+  }
   return (
     <BrowserRouter>
       <Header />
       <Routes>
         <Route path="/" element={<App />} />
-        <Route path="/products" element={<Main products={products} />} />
-        <Route path="/cart" element={<Cart />} />
+        <Route
+          path="/products"
+          element={<Main products={products} onAdd={onAdd} />}
+        />
+        <Route
+          path="/cart"
+          element={
+            <Cart cartItems={cartItems} onAdd={onAdd} onRemove={onRemove} />
+          }
+        />
       </Routes>
     </BrowserRouter>
   );

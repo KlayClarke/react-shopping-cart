8fc77e0458840a8913e2aa4957e2e87bb560cfaa
Klay Clarke <klayaclarke@gmail.com>
2022-02-24 22:28:17 -0500

    pass onAdd function as a prop from main page (houses all products) to each individual product div

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,14 +1,13 @@
 import Product from "./Product";
 
 export default function Main(props) {
-  const { products } = props;
-  console.log(products);
+  const { products, onAdd } = props;
   return (
     <main className="block col-2">
       <h2>Products</h2>
       <div className="row">
         {products.map((product) => (
-          <Product product={product} key={product.id}></Product>
+          <Product product={product} key={product.id} onAdd={onAdd}></Product>
         ))}
       </div>
     </main>

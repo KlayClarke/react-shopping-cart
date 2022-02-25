3b3c91c13e96004c1c4e7328008950ac081359f9
Klay Clarke <klayaclarke@gmail.com>
2022-02-24 21:33:44 -0500

    at the /products path, render Main component, passing in our data (products) as a prop

--- src/RouteSwitch.js
+++ src/RouteSwitch.js
@@ -1,14 +1,18 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import App from "./App";
+import Main from "./Components/Main";
 import Cart from "./Components/Cart";
 import Header from "./Components/Header";
+import data from "./data";
 
 export default function RouteSwitch() {
+  const { products } = data;
   return (
     <BrowserRouter>
       <Header />
       <Routes>
         <Route path="/" element={<App />} />
+        <Route path="/products" element={<Main products={products} />} />
         <Route path="/cart" element={<Cart />} />
       </Routes>
     </BrowserRouter>

f0df5b8de1c615abf603f2d64e2e4695138523be
Klay Clarke <klayaclarke@gmail.com>
2022-02-24 22:24:33 -0500

    retrieve props for cartitems, and functionality to add and remove items from cart - display subtotal that changes when the global state changes - if cart is empty, display empty message; else, for each cart item, display name, price, qty and buttons to increment and decrement qty

--- src/Components/Cart.js
+++ src/Components/Cart.js
@@ -1,7 +1,37 @@
 export default function Cart(props) {
+  const { cartItems, onAdd, onRemove } = props;
+  let total = 0;
+
+  for (let item of cartItems) {
+    total += item.price * item.qty;
+  }
   return (
     <aside className="block col-1">
+      <h1>Subtotal: ${total}</h1>
       <h2>Cart Items</h2>
+      <div>
+        {cartItems.length === 0 && (
+          <>
+            <div>Cart Is Empty</div>
+          </>
+        )}
+      </div>
+      {cartItems.map((item) => (
+        <div key={item.id} className="row">
+          <div className="col-2">{item.name}</div>
+          <div className="col-2">
+            <button onClick={() => onAdd(item)} className="add">
+              +
+            </button>
+            <button onClick={() => onRemove(item)} className="remove">
+              -
+            </button>
+          </div>
+          <div className="col-2 text-right">
+            {item.qty} x ${item.price.toFixed(2)}
+          </div>
+        </div>
+      ))}
     </aside>
   );
 }

b2d1418cd0aae95de5c0a59b5303a0e0a02b6991
Klay Clarke <klayaclarke@gmail.com>
2022-02-24 23:14:44 -0500

    add empty alt tag for product imgs (will add content later)

--- src/Components/Product.js
+++ src/Components/Product.js
@@ -2,7 +2,7 @@ export default function Product(props) {
   const { product, onAdd } = props;
   return (
     <div>
-      <img className="small" src={product.image}></img>
+      <img className="small" src={product.image} alt=" "></img>
       <h4 className="product-name">{product.name}</h4>
       <div>${product.price}</div>
       <div>

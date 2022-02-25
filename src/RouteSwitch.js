628e6e3a0520013bfa10a048b67bf2d9d6ceb568
Klay Clarke <klayaclarke@gmail.com>
2022-02-24 23:16:47 -0500

    add functionality to onRemove func (allows user to remove associated cartItem / qty from cart) - if item qty is equal to 1 at time of onRemove, remove item from cart completely - else, decrement the items qty number

--- src/RouteSwitch.js
+++ src/RouteSwitch.js
@@ -38,12 +38,12 @@ export default function RouteSwitch() {
 
   function onRemove(product) {
     const exist = cartItems.find((x) => x.id === product.id);
-    if (exist) {
+    if (exist.qty === 1) {
+      setCartItems(cartItems.filter((x) => x.id !== product.id));
+    } else if (exist) {
       setCartItems(
         cartItems.map((x) =>
-          x.id === product.id && x.qty > 0
-            ? { ...exist, qty: exist.qty - 1 }
-            : x
+          x.id === product.id ? { ...exist, qty: exist.qty - 1 } : x
         )
       );
     }
